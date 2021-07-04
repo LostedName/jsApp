bca3ad83c0f6e24583702c053b8efa6a457d7147
LostedName <Dizikworld123@gmail.com>
2021-07-05 02:37:59 +0300

    Middleware test, its cool!

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ import {PORT} from './settings.js'
 import {User,Run} from './dbController.js'
 const app = express();
 app.use(express.json());
-User.create({
-        login: "Tomy",
-        password: "12345",
-        reg_date:  Date().toString(),
-        photo: "photo"
-      }).then(res=>{
-        console.log(res);
-      }).catch(err=>console.log(err));
-app.route('/')
-.get((req,res)=>{})
-.post((req,res)=>{});
+// User.create({
+//         login: "Tomy",
+//         password: "12345",
+//         reg_date:  Date().toString(),
+//         photo: "photo"
+//       }).then(res=>{
+//         console.log(res);
+//       }).catch(err=>console.log(err));
+// app.route('/')
+// .get((req,res)=>{})
+// .post((req,res)=>{});
+app.use(auth);
+app.get('/',(req,res)=>{
+    res.send("You use Get");
+});
+app.post('/',log,log,log,log,auth,(req,res)=>{
+    res.send("You use Post");
+});
+function auth(req,res,next){
+    console.log("auth");
+    next();
+}
+function log(req,res,next){
+console.log("log");
+    next();
+}
 app.listen(PORT, ()=> console.log(`Server is started on port: ${PORT}`));
